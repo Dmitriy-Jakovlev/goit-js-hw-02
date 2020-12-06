1393708dd158ec076d04c591a25f4560f88a7353
Dmitriy-Jakovlev <dimas39690023@gmail.com>
2020-12-06 12:31:22 +0200

    completed the task

---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+'use script';
+
+const checkForSpam = function (message) {
+    let messageForCheck = message.toLowerCase();
+    return messageForCheck.includes('spam') || messageForCheck.includes('sale');
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
